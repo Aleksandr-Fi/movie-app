ea8a1acb94c6925202456111f0acbec0520be2c6
Aleksandr-Fi <90216724+Aleksandr-Fi@users.noreply.github.com>
2022-02-17 00:32:21 +0300

    Update FilmCard.jsx
    -add Rate
    - fix getShortText
    
    // have bug to hieroglyphic title

--- src/components/FilmCard/FilmCard.jsx
+++ src/components/FilmCard/FilmCard.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import { format } from 'date-fns'
-import { Card } from 'antd'
+import { Card, Rate } from 'antd'
 
 import GenresList from '../GenresList'
 
@@ -11,24 +11,36 @@ export default class FilmCard extends Component {
     releaseDate: this.props.release_date,
     genre: this.props.genre_ids,
     overview: this.props.overview,
+    rate: {
+      value: 0,
+    },
   }
 
-  getShortText(str, n) {
+  getShortText(str, title) {
+    let n = 160
+    if (title.length > 24 && title.length < 48) {
+      n = 130
+    }
+    if (title.length > 48) {
+      n = 100
+    }
     const shortText = str.length > n ? str.substr(0, n - 1) + '&hellip;' : str
     const arrayWords = shortText.split(' ')
     return `${arrayWords.slice(0, -1).join(' ')} ...`
   }
 
   render() {
-    const date = this.state.releaseDate
+    const { poster, title, releaseDate, genre, overview, rate } = this.state
+    console.log(this.state.title.length + ' ' + this.state.title)
     return (
       <Card className="film-card" hoverable style={{ width: 454, height: 280 }}>
-        <img className="film-poster" alt="poster" src={`https://image.tmdb.org/t/p/w185${this.state.poster}`} />
+        <img className="film-poster" alt="poster" src={`https://image.tmdb.org/t/p/w185${poster}`} />
         <div className="film-content">
-          <h1 className="film-title">{this.state.title}</h1>
-          <span className="film-date">{date ? format(new Date(date), 'PP') : null}</span>
-          <GenresList genreIds={this.state.genre} />
-          <p className="film-overview">{this.getShortText(this.state.overview, 160)}</p>
+          <h1 className="film-title">{title}</h1>
+          <span className="film-date">{releaseDate ? format(new Date(releaseDate), 'PP') : null}</span>
+          <GenresList genreIds={genre} />
+          <p className="film-overview">{this.getShortText(overview, title)}</p>
+          <Rate className="rate" count={10} value={rate} style={{ fontSize: 16, width: 239 }} />
         </div>
       </Card>
     )
